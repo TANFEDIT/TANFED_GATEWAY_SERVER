71e362892c4106a875ad5c0b1a5e05a3006ba02d
TANFEDIT <tanfeditteam@gmail.com>
2025-09-03 19:40:43 +0530

    tesi api

--- src/main/java/com/tanfed/gateway/GatewayServerApplication.java
+++ src/main/java/com/tanfed/gateway/GatewayServerApplication.java
@@ -3,10 +3,18 @@ package com.tanfed.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class GatewayServerApplication {
+	
+	@GetMapping("/test")
+	public String test() throws Exception {
+		return "Service Deployed Successfully!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
