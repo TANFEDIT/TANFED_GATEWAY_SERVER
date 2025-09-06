c11cfd050ad958e3a6966fae70c916aa240a245a
TANFEDIT <tanfeditteam@gmail.com>
2025-09-06 10:46:03 +0530

    acc config for depolyment

--- src/main/java/com/tanfed/gateway/GatewayServerApplication.java
+++ src/main/java/com/tanfed/gateway/GatewayServerApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GatewayServerApplication {
 	
-	@GetMapping("/test")
+	@GetMapping("/")
 	public String test() throws Exception {
 		return "Service Deployed Successfully!";
 	}
